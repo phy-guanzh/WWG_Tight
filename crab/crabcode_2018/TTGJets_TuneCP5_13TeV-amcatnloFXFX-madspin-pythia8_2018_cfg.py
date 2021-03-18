1661e136d6623d623f80f29d11188ec33ee87fb2
Ying An <ying.an@cern.ch>
2021-03-18 05:29:59 +0100

    update

---
+++ crab/crabcode_2018/TTGJets_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8_2018_cfg.py
@@ -0,0 +1,34 @@
+from WMCore.Configuration import Configuration 
+
+config = Configuration()
+config.section_("General")
+config.General.requestName = "TTGJets_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8_2018"
+config.General.transferLogs = False 
+config.General.workArea = "crab2018"
+
+config.section_("JobType")
+config.JobType.pluginName = "Analysis"
+config.JobType.psetName = "PSet.py"
+config.JobType.scriptExe = "./WWG_crab_script.sh" 
+config.JobType.inputFiles = ["../../scripts/haddnano.py","../WWG_selector/WWG_postproc.py","../WWG_selector/WWG_Module.py","../WWG_selector/WWG_keep_and_drop.txt","../WWG_selector/WWG_output_branch.txt","../WWG_selector/DAS_filesearch.py"] #hadd nano will not be needed once nano tools are in cmssw 
+config.JobType.scriptArgs = ["isdata=MC","year=2018"] 
+config.JobType.sendPythonFolder  = True
+config.JobType.allowUndistributedCMSSW = True 
+
+config.section_("Data")
+config.Data.inputDataset = "/TTGJets_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8/RunIISummer19UL18NanoAODv2-106X_upgrade2018_realistic_v15_L1v1-v1/NANOAODSIM" 
+#config.Data.inputDBS = "phys03"
+config.Data.inputDBS = "global"
+# config.Data.splitting = "LumiBased"
+config.Data.splitting = "FileBased"
+#config.Data.splitting = "EventAwareLumiBased" 
+#config.Data.splitting = "Automatic" 
+config.Data.unitsPerJob = 1
+config.Data.publication = False
+config.Data.ignoreLocality = True
+config.Data.allowNonValidInputDataset = True
+config.Data.outputDatasetTag = "TTGJets_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8_2018" 
+
+config.section_("Site")
+config.Site.storageSite = "T3_CH_CERNBOX"
+config.Site.whitelist = ["T2_US_MIT","T2_US_Wisconsin","T2_US_Purdue","T2_US_UCSD","T2_US_Caltech","T2_US_Nebraska"] 
