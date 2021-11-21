55928fed4e135cac3acee31b5be0611ba3c2a2e4
Ying An <ying.an@cern.ch>
2021-11-21 11:48:20 +0100

    update

--- 2016pre/WWG_selector/WWG_postproc.py
+++ 2016pre/WWG_selector/WWG_postproc.py
@@ -19,7 +19,7 @@ parser.add_argument('-f', dest='infile', default='', help='local file input')
 parser.add_argument('-y', dest='year', default='2016', help='year of dataset')
 parser.add_argument('-m', dest='mode', default='local', help='runmode local/condor')
 parser.add_argument('-d', dest='isdata',action='store_true',default=False)
-parser.add_argument('-e', dest='era',default='A')
+parser.add_argument('-e', dest='era',default='B')
 args = parser.parse_args()
 
 print "mode: ", args.mode
@@ -32,12 +32,12 @@ from PhysicsTools.NanoAODTools.postprocessing.modules.common.PrefireCorr import 
 
 if args.year=='2016':
    PrefCorr_2016 = lambda: PrefCorr("L1prefiring_jetpt_2016BtoH.root","L1prefiring_jetpt_2016BtoH","L1prefiring_photonpt_2016BtoH.root","L1prefiring_photonpt_2016BtoH")
-   jmeCorrections_ak4_MC = createJMECorrector(True,2016,"A","Total","AK4PFchs",False,"PuppiMET",True,False,True,True)
+   jmeCorrections_ak4_MC = createJMECorrector(True,2016,"B","Total","AK4PFchs",False,"PuppiMET",True,False,True,True)
    jmeCorrections_ak4_Data = createJMECorrector(False,2016,args.era,"Total","AK4PFchs",False,"PuppiMET",True,False,True,True)
    btagSF = lambda: btagSFProducer("2016",'deepcsv')
 if args.year=='2017':
    PrefCorr_2017 = lambda: PrefCorr("L1prefiring_jetpt_2017BtoF.root","L1prefiring_jetpt_2017BtoF","L1prefiring_photonpt_2017BtoF.root","L1prefiring_photonpt_2017BtoF")
-   jmeCorrections_ak4_MC = createJMECorrector(True,2017,"A","Total","AK4PFchs",False,"PuppiMET",True,False,True,True)
+   jmeCorrections_ak4_MC = createJMECorrector(True,2017,"B","Total","AK4PFchs",False,"PuppiMET",True,False,True,True)
    jmeCorrections_ak4_Data = createJMECorrector(False,2017,args.era,"Total","AK4PFchs",False,"PuppiMET",True,False,True,True)
    btagSF = lambda: btagSFProducer("2017",'deepcsv')
 if args.year=='2018':
